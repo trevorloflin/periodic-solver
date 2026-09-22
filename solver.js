dbd56e8a8e47167b0fb5fb7649967041f90f0a50
Trevor Loflin <trevorloflin@users.noreply.github.com>
2026-09-22 10:13:15 -0600

    Create solver.js

---
+++ solver.js
@@ -0,0 +1,11 @@
+function Solve() {
+  let ciphertext = document.getElementById("ciphertext");
+  let plaintext = document.getElementById("plaintext");
+
+  
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let solveBtn = document.getElementById("solve-btn");
+  solveBtn.addEventListener("click", Solve);
+});
